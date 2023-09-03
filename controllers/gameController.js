e78e4c1dc2357458838e33fea80c71a18d4bceb9
wojciechguze2 <wojciechguze2@gmail.com>
2023-09-03 17:14:33 +0200

    added creating game (with values) endpoint + test http request

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,25 +1,41 @@
 const db = require("../models")
 
 const getGames = async (req, res) => {
-    try {
-        const games = await db.Game.findAll({
-            include: [
-                {
-                    model: db.GameValue
-                }
-            ],
-        });
+    const games = await db.Game.findAll({
+        include: [
+            {
+                model: db.GameValue
+            }
+        ],
+    });
 
-        res.json(games);
-    } catch (error) {
-        console.error(error);
+    res.json(games);
+};
 
-        res.status(500).json({
-            error: 'getGames 500'
-        });
-    }
+const createGame = async (req, res) => {
+    const {
+        gameName,
+        gameValues
+    } = req.body;
+
+    const game = await db.Game.create({
+        name: gameName,
+    });
+
+    await Promise.all(
+        gameValues.map(async (value) => {
+            await db.GameValue.create({
+                value: value.value,
+                chance: value.chance,
+                gameId: game.id,
+            });
+        })
+    );
+
+    res.status(201).json(game);
 };
 
 module.exports = {
     getGames,
+    createGame,
 };
--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const gameController = require('../controllers/gameController');
 
 router.get('/games', gameController.getGames);
+router.post('/games', gameController.createGame);
 
 module.exports = router;
