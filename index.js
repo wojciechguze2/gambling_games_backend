762ba9b2cd6aca7646a0f79ea0fd819379a23e3d
wojciechguze2 <wojciechguze2@gmail.com>
2023-09-03 17:04:39 +0200

    added basic error handling middleware

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require("./models");
+const errorHandler = require("./middleware/errorHandler");
 const app = express();
 const port = 8085;
 
 app.use(bodyParser.json());
+
 app.use('/api', require('./routes/gameRoutes'));
+app.use(errorHandler);
 
 db.sequelize.sync().then(() => {
     app.listen(port, () => {
