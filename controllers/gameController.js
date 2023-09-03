7d4216680c8a5867c33b2bd7f1b2aa33c11c60b2
wojciechguze2 <wojciechguze2@gmail.com>
2023-09-03 18:24:01 +0200

    added update, delete game endpoint

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -9,7 +9,7 @@ const getGames = async (req, res) => {
         ],
     });
 
-    res.json(games);
+    res.status(200).json(games);
 };
 
 const createGame = async (req, res) => {
@@ -35,7 +35,56 @@ const createGame = async (req, res) => {
     res.status(201).json(game);
 };
 
+const updateGame = async (req, res) => {
+    const {
+        gameId,
+        gameName,
+        gameValues
+    } = req.body;
+
+    const game = await db.Game.findByPk(gameId);
+
+    if (!game) {
+        return res.status(404).json({ error: 'Game does not exist.' });
+    }
+
+    if (gameName) {
+        game.name = gameName;
+        await game.save();
+    }
+
+    await db.GameValue.destroy({ where: { gameId } });
+
+    await Promise.all(
+        gameValues.map(async (value) => {
+            await db.GameValue.create({
+                value: value.value,
+                chance: value.chance,
+                gameId: gameId,
+            });
+        })
+    );
+
+    res.status(200).json(game);
+};
+
+const deleteGame = async (req, res) => {
+    const { gameId } = req.query;
+
+    const game = await db.Game.findByPk(gameId)
+
+    if (!game) {
+        return res.status(404).json({ error: 'Game does not exist.' });
+    }
+
+    await game.destroy();
+
+    res.status(204).send();
+};
+
 module.exports = {
     getGames,
     createGame,
+    updateGame,
+    deleteGame
 };
--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -4,5 +4,7 @@ const gameController = require('../controllers/gameController');
 
 router.get('/games', gameController.getGames);
 router.post('/games', gameController.createGame);
+router.delete('/games', gameController.deleteGame);
+router.patch('/games', gameController.updateGame);
 
 module.exports = router;
